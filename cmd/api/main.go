09d6cc49e69317fd7bb8f52ac1bf95f5dc339dbc
AndreReyesG <jandre.reyesg@gmail.com>
2026-03-17 10:11:01 -0600

    feat(api): crear CryptoMonitorServer y hacer una fake implementation InMemoryExchange

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"cryptomonitor/internal/api"
+	"cryptomonitor/internal/domain"
+)
+
+type InMemoryExchange struct{}
+
+func (i *InMemoryExchange) GetPrice(coin string) (domain.Price, error) {
+	return domain.Price{
+		Coin:     "dogecoin",
+		Currency: "mxn",
+		Value:    1.81,
+	}, nil
+}
 
 func main() {
-	fmt.Println("Hola desde la aplicación de API")
+	server := api.NewCryptoMonitorServer(&InMemoryExchange{})
+	log.Print("Iniciando servidor en el puerto :4000")
+	log.Fatal(http.ListenAndServe(":4000", server))
 }
--- internal/api/server.go
+++ internal/api/server.go
@@ -1,1 +1,40 @@
 package api
+
+import (
+	"encoding/json"
+	"net/http"
+	"strings"
+
+	"cryptomonitor/internal/domain"
+)
+
+type CryptoMonitorServer struct {
+	exchange domain.Exchange
+}
+
+func NewCryptoMonitorServer(e domain.Exchange) *CryptoMonitorServer {
+	return &CryptoMonitorServer{
+		exchange: e,
+	}
+}
+
+func (c *CryptoMonitorServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	coin := strings.TrimPrefix(r.URL.Path, "/v1/prices/")
+
+	price, err := c.exchange.GetPrice(coin)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		msg := struct {
+			ErrMsg string `json:"error"`
+		}{
+			ErrMsg: "oh no",
+		}
+
+		js, _ := json.Marshal(msg)
+		w.Write(js)
+		return
+	}
+
+	js, _ := json.Marshal(price)
+	w.Write(js)
+}
