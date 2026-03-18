f05304c977de4280e1614ab233be17058206d99e
AndreReyesG <jandre.reyesg@gmail.com>
2026-03-17 23:34:27 -0600

    test(webserver): probar el renderizado en internet
    Esto es para verificar que el metodo ui.Render funciona cuando se
    escribe en internet.

--- cmd/webserver/main.go
+++ cmd/webserver/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"cryptomonitor/internal/domain"
+	"cryptomonitor/ui"
+)
 
 func main() {
-	fmt.Println("Hola desde el servidor web")
+	coins := []domain.Price{
+		{
+			Coin:        "bitcoin",
+			Value:       1309186,
+			LastUpdated: time.Now(),
+			Exchange:    "coingecko",
+		},
+		{
+			Coin:        "ethereum",
+			Value:       40815,
+			LastUpdated: time.Now(),
+			Exchange:    "coingecko",
+		},
+	}
+
+	dashboard, err := ui.NewRenderer()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	router := http.NewServeMux()
+
+	router.Handle("/{$}", http.HandlerFunc(func(w http.ResponseWriter, h *http.Request) {
+		if err := dashboard.Render(w, coins, "dashboard.html"); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	}))
+
+	log.Print("Iniciando servidor en el puerto :9000")
+	log.Fatal(http.ListenAndServe(":9000", router))
 }
