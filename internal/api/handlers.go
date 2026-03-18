65a47fda81fbc19ff13634dbeee88a5aca27e6e9
AndreReyesG <jandre.reyesg@gmail.com>
2026-03-17 21:05:26 -0600

    refactor(api): crear funcion writerJSON()

--- internal/api/handlers.go
+++ internal/api/handlers.go
@@ -1,7 +1,6 @@
 package api
 
 import (
-	"encoding/json"
 	"log"
 	"net/http"
 	"strings"
@@ -16,26 +15,17 @@ func (c *CryptoMonitorServer) showPriceHandler(w http.ResponseWriter, r *http.Re
 			"error": err.Error(),
 		}
 
-		js, err := json.Marshal(errMsg)
+		err = writeJSON(w, http.StatusNotFound, errMsg)
 		if err != nil {
 			log.Print(err.Error())
 			http.Error(w, "El servidor encontro un problema y no puede procesar tu request", http.StatusInternalServerError)
-			return
 		}
-
-		w.Header().Set("content-type", "application/json")
-		w.WriteHeader(http.StatusNotFound)
-		w.Write(js)
 		return
 	}
 
-	js, err := json.Marshal(price)
+	err = writeJSON(w, http.StatusOK, price)
 	if err != nil {
 		log.Print(err.Error())
 		http.Error(w, "El servidor encontro un problema y no puede procesar tu request", http.StatusInternalServerError)
-		return
 	}
-
-	w.Header().Set("content-type", "application/json")
-	w.Write(js)
 }
--- internal/api/server.go
+++ internal/api/server.go
@@ -15,7 +15,7 @@ func NewCryptoMonitorServer(e domain.Exchange) *CryptoMonitorServer {
 	c := new(CryptoMonitorServer)
 
 	c.exchange = e
-	
+
 	router := http.NewServeMux()
 	router.Handle("/v1/prices/", http.HandlerFunc(c.showPriceHandler))
 
