3a69255576a159535493fc8812d9c07550a82acc
AndreReyesG <jandre.reyesg@gmail.com>
2026-03-20 19:48:44 -0600

    feat(assert): agregar la funcion Price()
    Esta función compara que la información de dos domain.Price sean la
    misma.
    NOTA: Está función esta en internal/api/testing.go como AssertPrice().
    Se debe borrar dicha función para que no hay redundancia.

--- internal/assert/assert.go
+++ internal/assert/assert.go
@@ -3,6 +3,8 @@ package assert
 import (
 	"errors"
 	"testing"
+
+	"cryptomonitor/internal/domain"
 )
 
 func Error(t testing.TB, got, want error) {
@@ -13,5 +15,32 @@ func Error(t testing.TB, got, want error) {
 
 	if !errors.Is(got, want) {
 		t.Errorf("se obtuvo '%v'; se queria '%v'", got, want)
+	}
+}
+
+func Price(t testing.TB, got, want domain.Price) {
+	t.Helper()
+	if got.Coin != want.Coin {
+		t.Fatalf("no concuerda domain.Price.Coin; se obtuvo %q, se queria %q", got.Coin, want.Coin)
+	}
+
+	if got.Currency != want.Currency {
+		t.Fatalf("no concuerda domain.Price.Currency; se obtuvo %q, se queria %q",
+			got.Currency, want.Currency)
+	}
+
+	if got.Value != want.Value {
+		t.Fatalf("no concuerda domain.Price.Value; se obtuvo %f, se queria %f",
+			got.Value, want.Value)
+	}
+
+	if got.Exchange != want.Exchange {
+		t.Fatalf("no concuerda domain.Price.Exchange; se obtuvo %q, se queria %q",
+			got.Exchange, want.Exchange)
+	}
+
+	if got.LastUpdated != want.LastUpdated {
+		t.Fatalf("no concuerda domain.Price.LastUpdated; se obtuvo %v, se queria %v",
+			got.LastUpdated, want.LastUpdated)
 	}
 }
