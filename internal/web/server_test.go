2a9767513a24df2a0ac7951514d3261d76704a86
AndreReyesG <jandre.reyesg@gmail.com>
2026-03-26 11:44:07 -0600

    test(web): crear newTestServer()
    ¿newTestServer() es un proxy?
    newTestServer() ayuda a descartar el logger en las
    pruebas.
    También, se elimino la variable dummyStrSlice.

--- internal/web/server_test.go
+++ internal/web/server_test.go
@@ -1,19 +1,29 @@
 package web_test
 
 import (
+	"log/slog"
 	"testing"
 	"time"
 
 	"cryptomonitor/internal/assert"
 	"cryptomonitor/internal/domain"
-	platformhttp "cryptomonitor/internal/platform/http"
+	phttp "cryptomonitor/internal/platform/http"
 	"cryptomonitor/internal/web"
 )
 
 var (
-	fixedTime     = time.Date(2026, 1, 1, 0, 0, 0, 0, time.UTC)
-	dummyStrSlice = []string{}
+	fixedTime = time.Date(2026, 1, 1, 0, 0, 0, 0, time.UTC)
 )
+
+// NOTE: ¿Esto es un Proxy?
+func newTestServer(t *testing.T, c phttp.Client) *web.CryptoMonitorServer {
+	logger := slog.New(slog.DiscardHandler)
+	server, err := web.NewCryptoMonitorServer(c, []string{}, logger)
+	if err != nil {
+		t.Fatalf("problema creado el servidor, %q", err.Error())
+	}
+	return server
+}
 
 const (
 	btcJSON = `{"coin":"bitcoin","currency":"mxn","value":1264773,"exchange":"coingecko","last_updated":"2026-01-01T00:00:00Z"}`
@@ -55,8 +65,8 @@ func TestGETPrice(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			stubClient := platformhttp.NewStub(200, tt.body)
-			server, _ := web.NewCryptoMonitorServer(stubClient, dummyStrSlice)
+			stubClient := phttp.NewStub(200, tt.body)
+			server := newTestServer(t, stubClient)
 			got, err := server.GetPrice(tt.coin)
 			assert.NoError(t, err)
 			assert.Price(t, got, tt.expectedPrice)
